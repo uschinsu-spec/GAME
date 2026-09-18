0fac48daf3323a37e6f59980402b14a400c14a57
uschinsu-spec <uschinsu@gmail.com>
2026-09-18 14:36:16 +0700

    Bump service worker for crafting system rollout

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const C='tutien-v3';
+const C='tutien-v4-crafting';
 self.addEventListener('install',e=>{self.skipWaiting()});
 self.addEventListener('activate',e=>e.waitUntil(
   caches.keys().then(keys=>Promise.all(keys.map(k=>caches.delete(k)))).then(()=>self.clients.claim())
