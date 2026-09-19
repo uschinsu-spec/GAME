60ad745a80eec27e7343f5c7d824af47a0a0d7b4
uschinsu-spec <uschinsu@gmail.com>
2026-09-19 09:49:02 +0700

    Refresh service worker caches for v87

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const BUILD='20260919-v86';
-const CACHE={shell:'game-shell-v86',assets:'game-assets-v86',maps:'game-maps-v86'};
+const BUILD='20260919-v87';
+const CACHE={shell:'game-shell-v87',assets:'game-assets-v87',maps:'game-maps-v87'};
 const OWN=new Set(Object.values(CACHE));
 const SHELL=['./','./index.html','./style.css','./manifest.webmanifest','./build.json'];
 
