78251bb35276bde12edd09d42126f5ee4ef47cce
uschinsu-spec <uschinsu@gmail.com>
2026-09-19 08:52:01 +0700

    chore: refresh service worker cache for NPC update

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const BUILD='20260918-v45';
-const CACHE={shell:'game-shell-v45',assets:'game-assets-v45',maps:'game-maps-v45'};
+const BUILD='20260919-v79';
+const CACHE={shell:'game-shell-v79',assets:'game-assets-v79',maps:'game-maps-v79'};
 const OWN=new Set(Object.values(CACHE));
 const SHELL=['./','./index.html','./style.css','./manifest.webmanifest','./build.json'];
 
